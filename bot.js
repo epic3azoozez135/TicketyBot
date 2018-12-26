c3c90af7a91496cdd4ed20d09292b60f99129248
Azoooooooooz <44898336+epic3azoozez135@users.noreply.github.com>
2018-12-26 14:23:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -48,7 +48,7 @@ client.on("message", (message) => {
   if (message.content.toLowerCase().startsWith(prefix + `new`)) {
       const reason = message.content.split(" ").slice(1).join(" ");
       if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`هذا السيرفر ليس لديه \`Support Team\` صنع رتبة, لذلك لن يتم فتح التذكرة.\nاذا كنت تمتلك administrator, إنشاء اسم بهذا الاسم بالضبط وإعطائه للمستخدمين الذين يمكنهم مشاهدة التذاكر.`);
-      if (message.guild.channels.exists("name", "ticket-" + ${client.user.username} )) return message.channel.send(`انت بالفعل لديك تذكره مفتوحه.`);
+      if (message.guild.channels.exists("name", "ticket-" + `${message.author.username}` )) return message.channel.send(`انت بالفعل لديك تذكره مفتوحه.`);
       message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
           let role = message.guild.roles.find("name", "Support Team");
           let role2 = message.guild.roles.find("name", "@everyone");
